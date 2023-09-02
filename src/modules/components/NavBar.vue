a8b3774eae2eefad7629661bd4fc36c4487f1948
Michael Garcia <msgr.2300290992@gmail.com>
2023-09-02 10:08:14 -0500

    Examen Final Progra Web Vue

---
+++ src/modules/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="nav-bar">
+    <router-link to="/tokens">Tokens</router-link>
+    <router-link to="/estudiantes">Estudiantes</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.nav-bar {
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+  position: fixed;
+  top: 0;
+  left:0;
+  width: 100%;
+  z-index: 1000;
+}
+
+.nav-bar a {
+  text-decoration: none;
+  color: #fff;
+  margin-right: 20px;
+}
+
+.nav-bar a:hover {
+  color: #ff6600;
+}
+</style>
---
+++ src/modules/components/TablaEstudiantes.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tabla">
+    <table border="1" v-if="lista">
+      <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Cedula</th>
+      </tr>
+      <tr v-for="item in lista" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.nombre }}</td>
+        <td>{{ item.apellido }}</td>
+        <td>{{ item.cedula }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    estudiantes: {
+      type: Array,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      lista: this.estudiantes,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.tabla  {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.tabla table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #000;
+}
+
+.tabla th {
+  background-color: #7cdcc3;
+}
+.tabla td {
+  background-color: #bae2f5;
+}
+</style>
